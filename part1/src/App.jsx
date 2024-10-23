6193d77213a438caba6c928685e9adbce5cf2120
Mesadova <emesadova@hotmail.com>
2024-10-23 13:00:59 +0200

    1.12.Anecdotes

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,71 +1,34 @@
 import { useState } from 'react'
 
-const Button = ({handleEvent, text}) => {
-  return(
-    <button onClick={handleEvent}>{text}</button>
-  )
-}
-
-const Statistics = (props) => {
-  return(
-    <table>
-      <StatisticLine text='good' value={props.good}/>
-      <StatisticLine text='neutral' value={props.neutral}/>
-      <StatisticLine text='bad' value={props.bad}/>
-      <StatisticLine text='total' value={props.total}/>
-      <StatisticLine text='average' value={props.average}/>
-      <StatisticLine text='positive' value={props.positive}/>
-    </table>
-  )
-}
-
-const StatisticLine = ({text, value}) => {
-  return(
-    <tr>
-      <td>{text}:</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const handleGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
+  const handleRandom = () => {
+    const random = Math.floor(Math.random() * 8)
+
+    if (selected !== random) {
+      setSelected(random)
+    } else {
+      handleRandom()
+    }
+    console.log(random)
   }
-
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-  }
-
-  const handleBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-  }
-
-  const average = () => ((good) + (neutral * 0) + (bad * -1)) / total + ' %'
-
-  const positive = () => (good / (total)) * 100
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleEvent={handleGood} text='good'/>
-      <Button handleEvent={handleNeutral} text='neutral'/>
-      <Button handleEvent={handleBad} text='bad'/>
-
-      <h1>statistics</h1>
-      {total === 0 ? (
-        <p>No feedback given</p>
-      ) : (
-          <Statistics good={good} bad={bad} neutral={neutral} total={total} average={average()} positive={positive()}/>
-      )}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleRandom}>next anecdote</button>
     </div>
   )
 }
