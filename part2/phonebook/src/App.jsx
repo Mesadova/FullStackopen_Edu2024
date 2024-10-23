63e932ed3e0e29a6d4e503e4998dcd98e037d0ad
Mesadova <emesadova@hotmail.com>
2024-10-23 17:43:31 +0200

    2.9.Phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
+  
 
   const handleNewNames = (event) => {
     event.preventDefault()
@@ -26,9 +32,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    const updatedSearch = event.target.value
+    setNameFilter(updatedSearch)
+    const re = new RegExp('^' + updatedSearch, 'i')
+    const filteredPersonsCopy = persons.filter((person) => person.name.search(re) != -1)
+    setFilteredPersons(filteredPersonsCopy)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={nameFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleNewNames}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -41,9 +59,18 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <p key={person.name}>{person.name} {person.number}</p>
+      {nameFilter === '' ? (
+        persons.map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
+        )
+      ) : (
+        filteredPersons.map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
+        )
+        
       )}
+
+      
     </div>
   )
 }
