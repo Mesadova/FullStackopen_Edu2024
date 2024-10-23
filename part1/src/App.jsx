dd0dfd007811ac2489755602e8e034bc9416bffb
Mesadova <emesadova@hotmail.com>
2024-10-23 12:18:07 +0200

    1.7.Unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
@@ -20,6 +21,11 @@ const App = () => {
     setTotal(total + 1)
   }
 
+  const average = () => ((good) + (neutral * 0) + (bad * -1)) / total
+
+
+  const positive = () => (good / (total)) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -30,6 +36,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>total: {total}</p>
+      <p>average: {average()}</p>
+      <p>positive: {positive()} %</p>
     </div>
   )
 }
